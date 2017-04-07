c611690aefc862bc36ee6f040060ed7662e73143
baljoz <baki94@hotmail.rs>
2017-04-07 18:14:42 +0200

    Zavrsen login screan ali bez laiout

--- BalanS/ViewController.swift
+++ BalanS/ViewController.swift
@@ -12,17 +12,34 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    @IBOutlet weak var login: UIButton!
    
+    @IBOutlet weak var remember: UIButton!
+    @IBOutlet weak var loginView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-           }
+        login.layer.cornerRadius = 2;
+        login.layer.borderWidth = 1;
+        login.layer.borderColor = UIColor.black.cgColor
+        login.layer.cornerRadius=10
+        loginView.layer.cornerRadius=10
+        remember.layer.cornerRadius=5
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var checkButton: UIButton!
+    
+    @IBAction func OnCheck(_ sender: Any) {
+        
+        var imag = UIImage(named:"Checke")
+        
+        checkButton.setImage(imag, for: UIControlState.normal)
+    }
 
 }
 
